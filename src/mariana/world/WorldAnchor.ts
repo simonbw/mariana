271ee9d710eb6d33458559e7cd488af13603e2a1
Philip Hann <phann92@gmail.com>
2021-05-06 15:23:44 -0500

    Load and unload tiles proposal

--- src/mariana/world/WorldAnchor.ts
+++ src/mariana/world/WorldAnchor.ts
@@ -1,7 +1,8 @@
 import BaseEntity from "../../core/entity/BaseEntity";
 import Entity from "../../core/entity/Entity";
+import { SubGrid } from "../../core/util/SubGrid";
 import { V, V2d } from "../../core/Vector";
-import { ChunkPos, TilePos, WorldMap } from "./WorldMap";
+import { WorldMap } from "./WorldMap";
 
 /** Keeps certain tiles loaded */
 export class WorldAnchor extends BaseEntity implements Entity {
@@ -16,31 +17,10 @@ export class WorldAnchor extends BaseEntity implements Entity {
   }
 
   /** Returns a list of tiles that should stay loaded right now */
-  getTilesToLoad(map: WorldMap): TilePos[] {
-    const tiles: TilePos[] = [];
+  getTilesToLoad(map: WorldMap): SubGrid {
     const [x, y] = map.worldToTile(this.getCenter());
     const [minX, minY] = map.worldToTile(V(x - this.width, y - this.height));
-    const [maxX, maxY] = map.worldToTile(V(x + this.width, y + this.height));
-    for (let i = minX; i <= maxX; i++) {
-      for (let j = minY; j <= maxY; j++) {
-        tiles.push([x + i, y + j]);
-      }
-    }
-    return tiles;
-  }
-
-  /** Returns a list of tiles that should stay loaded right now */
-  getChunksToLoad(map: WorldMap): ChunkPos[] {
-    const chunks: ChunkPos[] = [];
-    const [x, y] = map.worldToChunk(this.getCenter());
-    const [minX, minY] = map.worldToChunk(V(x - this.width, y - this.height));
-    const [maxX, maxY] = map.worldToChunk(V(x + this.width, y + this.height));
-    for (let i = minX; i <= maxX; i++) {
-      for (let j = minY; j <= maxY; j++) {
-        chunks.push([x + i, y + j]);
-      }
-    }
-    return chunks;
+    return new SubGrid(minX, minY, this.width, this.height);
   }
 }
 
