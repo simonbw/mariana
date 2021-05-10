4ec0e0c100649dc98fba836d06c97c57588d59df
Simon Baumgardt-Wellander <simonbw@uw.edu>
2021-05-10 13:50:50 -0700

    harpoon splash

--- src/mariana/weapons/Harpoon.ts
+++ src/mariana/weapons/Harpoon.ts
@@ -1,15 +1,18 @@
-import { Body, Box, ContactEquation, Shape, vec2 } from "p2";
+import { Body, Box, vec2 } from "p2";
 import { Sprite } from "pixi.js";
 import img_harpoon from "../../../resources/images/diver/harpoon.png";
 import BaseEntity from "../../core/entity/BaseEntity";
 import Entity, { GameSprite } from "../../core/entity/Entity";
+import Game from "../../core/Game";
 import { clamp, polarToVec } from "../../core/util/MathUtil";
 import { rBool, rDirection, rNormal, rUniform } from "../../core/util/Random";
 import { V, V2d } from "../../core/Vector";
 import { CollisionGroups } from "../config/CollisionGroups";
 import { Bubble } from "../effects/Bubble";
+import { SurfaceSplash } from "../effects/SurfaceSplash";
+import { getWaves } from "../effects/Waves";
 import { isHarpoonable } from "./Harpoonable";
-import { DAMPING, SIZE } from "./HarpoonGun";
+import { SIZE } from "./HarpoonGun";
 
 const MIN_SPEED_FOR_DAMAGE = 5; // meters/second
 const FRICTION = 0.04;
@@ -20,7 +23,9 @@ export class Harpoon extends BaseEntity implements Entity {
 
   minSpeed = Infinity;
 
-  constructor(public position: V2d, public velocity: V2d) {
+  wasSurfaced: boolean = false;
+
+  constructor(position: V2d, public velocity: V2d) {
     super();
 
     const sprite = (this.sprite = Sprite.from(img_harpoon));
@@ -46,28 +51,44 @@ export class Harpoon extends BaseEntity implements Entity {
     this.body.angularDamping = 0.12;
   }
 
+  onAdd(game: Game) {
+    const [x, y] = this.body.position;
+    this.wasSurfaced = getWaves(game).getSurfaceHeight(x) > y;
+  }
+
   getVelocity(): V2d {
     return V(this.body.velocity);
   }
 
   onTick() {
+    const [x, y] = this.body.position;
+    const isSurfaced = getWaves(this.game!).getSurfaceHeight(x) > y;
+
+    if (isSurfaced != this.wasSurfaced) {
+      const speed = vec2.length(this.body.velocity);
+      this.game!.addEntity(new SurfaceSplash(x, speed / 5, 20));
+    }
+
     this.body.applyForce([0, 9.8 * this.body.mass]);
-    if (this.body.position[1] > 0) {
+    if (!isSurfaced) {
       this.body.applyForce(V(this.body.velocity).imul(-FRICTION));
+
+      const bubbleChance = clamp((this.minSpeed / 40) ** 2);
+      if (rBool(bubbleChance)) {
+        this.game!.addEntity(
+          new Bubble(
+            this.getPosition().iadd(
+              polarToVec(rDirection(), rUniform(0, 0.15))
+            ),
+            V(rNormal(), rNormal()),
+            rUniform(0.1, 0.2 + 0.2 * bubbleChance)
+          )
+        );
+      }
     }
 
+    this.wasSurfaced = isSurfaced;
     this.minSpeed = Math.min(this.minSpeed, vec2.length(this.body.velocity));
-
-    const bubbleChance = clamp((this.minSpeed / 40) ** 2);
-    if (rBool(bubbleChance)) {
-      this.game!.addEntity(
-        new Bubble(
-          this.getPosition().iadd(polarToVec(rDirection(), rUniform(0, 0.15))),
-          V(rNormal(), rNormal()),
-          rUniform(0.1, 0.2 + 0.2 * bubbleChance)
-        )
-      );
-    }
   }
 
   onRender() {
@@ -75,12 +96,7 @@ export class Harpoon extends BaseEntity implements Entity {
     this.sprite.rotation = this.body.angle - Math.PI / 4;
   }
 
-  onBeginContact(
-    other: Entity,
-    shapeA: Shape,
-    shapeB: Shape,
-    equations: ContactEquation[]
-  ) {
+  onBeginContact(other: Entity) {
     // harpoon other stuff
     if (isHarpoonable(other)) {
       other.onHarpooned(this);
