9f8bad180b3a00b01508dd9523838163563d758b
Simon Baumgardt-Wellander <simonbw@uw.edu>
2021-05-11 16:46:34 -0700

    grabby

--- src/mariana/plants/Grabber.ts
+++ src/mariana/plants/Grabber.ts
@@ -2,7 +2,12 @@ import { Graphics } from "@pixi/graphics";
 import { Body, LinearSpring, RotationalSpring } from "p2";
 import BaseEntity from "../../core/entity/BaseEntity";
 import Entity, { GameSprite } from "../../core/entity/Entity";
-import { V2d } from "../../core/Vector";
+import { last } from "../../core/util/FunctionalUtils";
+import { lerp } from "../../core/util/MathUtil";
+import { V, V2d } from "../../core/Vector";
+import { getDiver } from "../diver/Diver";
+
+const NUM_BODIES = 10;
 
 export class Grabber extends BaseEntity implements Entity {
   sprite: Graphics & GameSprite;
@@ -15,9 +20,9 @@ export class Grabber extends BaseEntity implements Entity {
     this.bodies = [];
     this.springs = [];
 
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < NUM_BODIES; i++) {
       const body = new Body({
-        position: position.add([0, 1]),
+        position: position.add([i, 0]),
         mass: 0.1,
       });
       this.bodies.push(body);
@@ -31,8 +36,25 @@ export class Grabber extends BaseEntity implements Entity {
 
       const linearSpring = new LinearSpring(bodyA, bodyB);
       const rotationalSpring = new RotationalSpring(bodyA, bodyB);
-      // this.springs.push(linearSpring, rotationalSpring);
+      this.springs.push(linearSpring, rotationalSpring);
     }
+  }
+
+  onTick() {
+    const diver = getDiver(this.game!);
+    if (diver) {
+      const tip = last(this.bodies!);
+
+      const direction = diver.getPosition().sub(this.getTip());
+      direction.inormalize(10);
+      tip.applyForce(direction);
+    }
+  }
+
+  private _tip = V(0, 0);
+  getTip(): V2d {
+    const tip = last(this.bodies!).position;
+    return this._tip.set(tip[0], tip[1]);
   }
 
   onRender() {
@@ -41,17 +63,14 @@ export class Grabber extends BaseEntity implements Entity {
     const bodies = this.bodies!;
     this.sprite.moveTo(...bodies[0].position);
     for (let i = 1; i < bodies.length; i++) {
+      const t = i / bodies.length;
       const body = bodies[i];
-      this.sprite.lineStyle({ width: 0.1 + 0.1 * i, color: 0x00ff00 });
+      this.sprite.lineStyle({
+        width: lerp(1.0, 0.1, t),
+        color: 0x00cc00,
+        miterLimit: Infinity,
+      });
       this.sprite.lineTo(...body.position);
-    }
-
-    for (const body of bodies) {
-      this.sprite.lineStyle();
-      this.sprite.beginFill(0x00ff00);
-      const [x, y] = body.position;
-      this.sprite.drawCircle(x, y, 1);
-      this.sprite.endFill();
     }
   }
 }
